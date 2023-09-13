49fa7aa5a6e9a70388d6d2d7924c65012ae50a02
Zyad <zeyad.nafi@gmail.com>
2023-09-13 14:52:31 +0300

    added javascript

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,26 +1,59 @@
 function getComputerChoice() {
+    let result = Math.floor(Math.random() * (4 - 1) + 1);
 
-    return Math.floor(Math.random() * (4 - 1) + 1);
+    switch (result) {
+        case 1:
+            return 'rock';
+        case 2:
+            return 'paper';
+        case 3:
+            return 'scissors';
+    }
 }
-function playRound(playerSelection, computerSelection) {
-    if(playerSelection == "rock" && computerSelection ==2) {
-        return "you lose."
+
+function playRound(playerSelection) {
+    computerSelection = getComputerChoice();
+    computerChoiceText.innerHTML = computerSelection;
+    if (playerSelection == "rock" && computerSelection == "paper") {
+        gameStatus.innerHTML = "YOU LOSE";
+
     }
-    else if(playerSelection == "paper" && playerSelection == "scissors") {
-        return "you lose."
+    else if (playerSelection == "paper" && computerSelection == "scissors") {
+        gameStatus.innerHTML = "YOU LOSE";
+
     }
-    else if(playerSelection == "scissors" && playerSelection == "rock") {
-        return "you lose."
+    else if (playerSelection == "scissors" && computerSelection == "rock") {
+        gameStatus.innerHTML = "YOU LOSE";
     }
-    else if(playerSelection == computerSelection) {
-        return "It's a draw"
+    else if (playerSelection == computerSelection) {
+        gameStatus.innerHTML = "A DRAW";
     }
     else {
-        return"you win"
+        gameStatus.innerHTML = "YOU WIN!";
     }
+    textStatus.style.display = 'block';
 }
 
- playerSelection = prompt("Enter rock, paper or scissors: ");
-playerSelection = playerSelection.toLowerCase();
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+let gameStatus = document.querySelector('.status');
+let computerChoiceText = document.querySelector('.computerChoosed');
+let playerChoiceText = document.querySelector('.playerChoosed');
+
+let textStatus = document.querySelector('.text');
+
+
+
+document.querySelector('.rock').addEventListener("click", function () {
+    playerSelection = "rock";
+    playerChoiceText.innerHTML = 'rock';
+    playRound(playerSelection);
+});
+document.querySelector('.paper').addEventListener("click", function () {
+    playerSelection = "paper";
+    playerChoiceText.innerHTML = 'paper';
+    playRound(playerSelection);
+});
+document.querySelector('.scissors').addEventListener("click", function () {
+    playerSelection = "scissors";
+    playerChoiceText.innerHTML = 'scissors';
+    playRound(playerSelection);
+});
